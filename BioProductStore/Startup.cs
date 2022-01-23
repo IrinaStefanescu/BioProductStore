f965345257324b9a9f835ef21a4f25a86d040771
IrinaStefanescu <49494590+IrinaStefanescu@users.noreply.github.com>
2022-01-23 16:49:20 +0200

    startup.cs fixes + added jwt middleware (added jwtsecret to appsettings)

--- BioProductStore/Startup.cs
+++ BioProductStore/Startup.cs
@@ -1,10 +1,10 @@
+using System.Collections.Generic;
 using BioProductStore.Data;
-using BioProductStore.Repositories.CategoryRepository;
-using BioProductStore.Repositories.ExpeditionAddressRepository;
-using BioProductStore.Repositories.OrderRepository;
-using BioProductStore.Repositories.ProductRepository;
-using BioProductStore.Repositories.UserRepository;
+using BioProductStore.DataAccess;
+using BioProductStore.Models;
+using BioProductStore.Repositories;
 using BioProductStore.Services;
+using BioProductStore.Services.AuthenticationService;
 using BioProductStore.Services.CategoryService;
 using BioProductStore.Services.ExpeditionAddressService;
 using BioProductStore.Services.OrderService;
@@ -13,16 +13,11 @@ using BioProductStore.Utilities;
 using BioProductStore.Utilities.JWTUtils;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BioProductStore
 {
@@ -42,35 +37,60 @@ namespace BioProductStore
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BioProductsStore", Version = "v1" });
+                
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description =
+                        "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+    
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "User",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
             });
 
             services.AddDbContext<BioProductStoreContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             //Repositories:
             //it's created everytime a request it's been made
-            services.AddTransient<IUserRepository, UserRepository>();
-            services.AddTransient<ICategoryRepository, CategoryRepository>();
-            services.AddTransient<IProductRepository, ProductRepository>();
-            services.AddTransient<IOrderRepository, OrderRepository>();
-            services.AddTransient<IExpeditionAddressRepository, ExpeditionAddressRepository>();
-           // services.AddTransient<IOrderProductRelationRepository, OrderProductRelationRepository>();
+            services.AddTransient<IGenericRepository<User>, GenericRepository<User>>();
+            services.AddTransient<IGenericRepository<Category>, GenericRepository<Category>>();
+            services.AddTransient<IGenericRepository<Product>, GenericRepository<Product>>();
+            services.AddTransient<IGenericRepository<Order>, GenericRepository<Order>>();
+            services.AddTransient<IGenericRepository<ExpeditionAddress>, GenericRepository<ExpeditionAddress>>();
+
+            services.AddTransient<UnitOfWork>();
 
             services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+            services.AddAutoMapper(typeof(Startup));
             services.AddScoped<IJWTUtils, JWTUtils>();
-            services.AddScoped<IUserService, UserService>();
-
-            
 
             //Services:
-            services.AddTransient<IUserService, UserService>();
-            services.AddTransient<ICategoryService, CategoryService>();
-            services.AddTransient<IProductService, ProductService>();
-            services.AddTransient<IOrderService, OrderService>();
-            services.AddTransient<IExpeditionAddressService, ExpeditionAddressService>();
-           // services.AddTransient<IOrderProductRelationService, OrderProductRelationService>();
-            services.AddDbContext<BioProductStoreContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IExpeditionAddressService, ExpeditionAddressService>();
 
-            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -95,7 +115,7 @@ namespace BioProductStore
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseJwtAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
